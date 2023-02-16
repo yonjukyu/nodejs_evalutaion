74fcaf904994e3473561e1f3c1a254d35304504c
yonjukyu <yonjukyu2@gmail.com>
2023-02-16 09:23:40 +0100

    remove useles project

--- backend/events/sendMail.js
+++
@@ -1,8 +0,0 @@
-const event = require('events');
-let evenement = new event.EventEmitter();
-
-evenement.on('sendMail', function (params) {
-    console.log(`${params.message} ${params.email}`);
-});
-
-module.exports = evenement;
